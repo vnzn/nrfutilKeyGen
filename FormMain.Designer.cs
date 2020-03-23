2e4889eb1d02f10ee01e84795b96035637fbc038
Vince <Vincent.Shanghai@hotmail.com>
2020-03-23 20:22:06 +0800

    Initial Version.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace nrfutilKeyGen
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGenerateNew = new System.Windows.Forms.Button();
+            this.buttonGeneratePublic = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.openFileDialogPrivateKey = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonGenerateNew
+            // 
+            this.buttonGenerateNew.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGenerateNew.Location = new System.Drawing.Point(78, 61);
+            this.buttonGenerateNew.Name = "buttonGenerateNew";
+            this.buttonGenerateNew.Size = new System.Drawing.Size(226, 87);
+            this.buttonGenerateNew.TabIndex = 0;
+            this.buttonGenerateNew.Text = "Generate New Private Key";
+            this.buttonGenerateNew.UseVisualStyleBackColor = true;
+            this.buttonGenerateNew.Click += new System.EventHandler(this.buttonGenerateNew_Click);
+            // 
+            // buttonGeneratePublic
+            // 
+            this.buttonGeneratePublic.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGeneratePublic.Location = new System.Drawing.Point(78, 199);
+            this.buttonGeneratePublic.Name = "buttonGeneratePublic";
+            this.buttonGeneratePublic.Size = new System.Drawing.Size(302, 131);
+            this.buttonGeneratePublic.TabIndex = 1;
+            this.buttonGeneratePublic.Text = "Generate Public Key from Private Key";
+            this.buttonGeneratePublic.UseVisualStyleBackColor = true;
+            this.buttonGeneratePublic.Click += new System.EventHandler(this.buttonGeneratePublic_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(13, 353);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxLog.Size = new System.Drawing.Size(509, 146);
+            this.textBoxLog.TabIndex = 2;
+            // 
+            // openFileDialogPrivateKey
+            // 
+            this.openFileDialogPrivateKey.Filter = "Key(*.key)|*.key";
+            this.openFileDialogPrivateKey.Title = "Select the Private Key";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.buttonGeneratePublic);
+            this.Controls.Add(this.buttonGenerateNew);
+            this.Name = "FormMain";
+            this.Text = "nrfutilKeyGen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerateNew;
+        private System.Windows.Forms.Button buttonGeneratePublic;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.OpenFileDialog openFileDialogPrivateKey;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,105 @@
+﻿using nrfjprogGUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nrfutilKeyGen
+{
+    public partial class FormMain : Form, CliInterface
+    {
+
+        private const String backupPrivateKeyPrefix = "move private.key private.key.";
+        private const String nrfutilCommandGeneratePrivateKey = "nrfutil.exe keys generate private.key";
+        private const String nrfutilCommandGeneratePublicKey = "nrfutil.exe keys display --key pk --format code private.key --out_file public_key.c";
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private delegate void updateLogDelegate(String log);
+        private void updateLog(String log)
+        {
+            try
+            {
+                if (this.InvokeRequired)
+                {
+                    this.Invoke(new updateLogDelegate(updateLog), new object[] { log });
+                }
+                else
+                {
+                    textBoxLog.Text = log;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void threadGenerate(object obj)
+        {
+
+            String selectedFileName = obj.ToString();
+
+
+            Cli c = new Cli(this);
+
+            if (string.IsNullOrEmpty(selectedFileName))
+            {
+
+                c.Execute(backupPrivateKeyPrefix + (System.DateTime.UtcNow.ToUniversalTime().Ticks / 10000000).ToString());
+                c.Execute(nrfutilCommandGeneratePrivateKey);
+            }
+
+
+            c.Execute(nrfutilCommandGeneratePublicKey);
+
+            Process proc = new Process();
+            proc.StartInfo.FileName = "explorer";
+            proc.StartInfo.Arguments = @"/select,"+ System.Windows.Forms.Application.ExecutablePath;
+            proc.Start();
+        }
+
+        public void CliInterfaceAnswerReceived(string answer)
+        {
+
+            updateLog(answer);
+        }
+
+        private void buttonGenerateNew_Click(object sender, EventArgs e)
+        {
+
+
+            Thread thread = new Thread(new ParameterizedThreadStart(threadGenerate));
+            thread.Start("");
+
+        }
+
+        private void buttonGeneratePublic_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialogPrivateKey.ShowDialog();
+            String selectedFileName = openFileDialogPrivateKey.FileName;
+            if (dr == System.Windows.Forms.DialogResult.OK && !string.IsNullOrEmpty(selectedFileName))
+            {
+
+                Thread thread = new Thread(new ParameterizedThreadStart(threadGenerate));
+                thread.Start(selectedFileName);
+            }
+            else
+            {
+                MessageBox.Show("Need to select a private key!");
+            }
+
+            openFileDialogPrivateKey.FileName = "";
+        }
+    }
+}
